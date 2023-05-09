5394004a15f19f293304dc705385fc6d80322048
Lucas Bubner <lkbubner@proton.me>
2023-05-09 19:32:02 +0930

    Content placeholders and potential layout design

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Lucas Bubner, 2023
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("readtime").innerHTML = getReadTime();
+    displayContent();
+});
+
+// Calculate how long it would take to read the article based on word count
+function getReadTime() {
+    const allSections = document.querySelectorAll(".text");
+    let wordCount = 0;
+    for (let i = 0; i < allSections.length; i++) {
+        wordCount += allSections[i].innerText.split(" ").length;
+    }
+    return Math.round(wordCount / 200) + " min read";
+}
+
+// Fade in content after page loads
+function displayContent() {
+    const allSections = document.querySelectorAll("html");
+    for (let i = 0; i < allSections.length; i++) {
+        allSections[i].style.opacity = "1";
+    }
+}
